7bd779c3f0b964a1ab6d7584522fdaa738b0b92c
Myduzo <sahli.youssef@outlook.com>
2020-04-30 16:05:54 +0100

    import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
+	import sys
 
-argc = len(sys.argv)
-argv = sys.argv
+	argc = len(sys.argv)
+	argv = sys.argv
 
-print("{} ".format(argc - 1) + \
-	("argument" if argc == 2 else "arguments") + \
-	("." if argc == 1 else ":") + ("\n"), end='')
+	print("{} ".format(argc - 1) + \
+		("argument" if argc == 2 else "arguments") + \
+		("." if argc == 1 else ":") + ("\n"), end='')
 
 
-x = 1
-while x < argc:
-	print("{}: {}".format(x, argv[x]))
-	x += 1
+	x = 1
+	while x < argc:
+		print("{}: {}".format(x, argv[x]))
+		x += 1
