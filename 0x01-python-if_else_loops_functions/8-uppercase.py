ddad276be3a494c0531412ca2980379b1c59b411
Myduzo <sahli.youssef@outlook.com>
2020-04-30 22:31:17 +0100

    fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-	for x in range(len(str)):
-		s = ord(str[x]) - 32
-		print("{}".format(chr(s) if ord(str[x]) in range (97, 122) else str[x]), end='')
-	print("\n")
+    for x in range(len(str)):
+        s = ord(str[x]) - 32
+        print("{}".format(chr(s)
+              if ord(str[x]) in range(97, 122) else str[x]), end='')
