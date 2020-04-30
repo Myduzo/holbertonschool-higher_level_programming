70327712790348c65cdfaf724a4ef8e7ca14f928
Myduzo <sahli.youssef@outlook.com>
2020-04-30 16:08:51 +0100

    import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
+    import sys
 
-	argc = len(sys.argv)
-	argv = sys.argv
-	result = 0
+    argc = len(sys.argv)
+    argv = sys.argv
+    result = 0
 
-	if (argc == 1):
-		print(result)
+    if (argc == 1):
+        print(result)
 
-	if (argc > 1):
-		x = 1
-		while x < argc:
-			result += int(argv[x])
-			x += 1
-		print(result)
+    if (argc > 1):
+        x = 1
+        while x < argc:
+            result += int(argv[x])
+            x += 1
+        print(result)
