5680c3b2683c05ac40d8755ef49d02bbb531889c
Myduzo <sahli.youssef@outlook.com>
2020-05-05 17:48:07 +0100

    python_lists

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if not sentence:
+        return len(sentence), None
 
-    return (len(sentence), None) if not sentence else (len(sentence), sentence[0])
+    else:
+    return len(sentence), sentence[0]
