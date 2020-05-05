6144b1d962328cf1b31590db971db78522859ceb
Myduzo <sahli.youssef@outlook.com>
2020-05-05 14:42:20 +0100

    python_lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    x = 0
-    while x in range(len(my_list)):
-        print("{}\n".format(my_list[x]), end="")
-        x += 1
+    for x in my_list:
+        print("{}".format(x))
