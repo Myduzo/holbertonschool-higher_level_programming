e4588e2443567992738d34cbf692b0fa890c55d1
Myduzo <sahli.youssef@outlook.com>
2020-05-18 21:37:09 +0100

    print int err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,8 @@ def safe_print_integer_err(value):
     from sys import stderr
     try:
         print("{:d}\n".format(value), end="")
-        return True
+
     except Exception as error:
         print("Exception: {}".format(error), file=stderr)
         return False
+    return True
