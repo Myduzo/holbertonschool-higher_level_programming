a8b3d2a34700cc83a348e85a0a42f34678838aad
Myduzo <sahli.youssef@outlook.com>
2020-05-19 18:15:08 +0100

    getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""insert class"""
+
+
+class Square:
+    """instance attribute"""
+    pass
+
+    def __init__(self, size=0):
+        """init method"""
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area method"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """A method which is used for getting a value is decorated with @property"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """A method which is used for setting a value is decorated with @size.setter"""
+        if not(isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        return self.__size
