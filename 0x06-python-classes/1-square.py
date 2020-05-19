65616d672dc41a295c2471e52bda7e2f2c80d480
Myduzo <sahli.youssef@outlook.com>
2020-05-19 16:51:07 +0100

    method

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""insert class"""
+
+
+class Square:
+    """instance attribute"""
+    pass
+
+    def __init__(self, size):
+        "__init__ method / size is a private instance attribute"
+        self.__size = size
