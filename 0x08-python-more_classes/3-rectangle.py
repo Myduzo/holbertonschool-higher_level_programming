6cc4cd37f68d980b7facd71b5b693f709ab10a38
Myduzo <sahli.youssef@outlook.com>
2020-05-25 17:21:32 +0100

    rec with #

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""Empty class"""
+
+
+class Rectangle:
+    """A class name Rectangle"""
+    pass
+
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width"""
+        if not isinstance(width, int):
+            raise TypeError("width must be integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Public instance method area"""
+        area = self.__width * self.__height
+        return area
+
+    def perimeter(self):
+        """Public instance method perimeter"""
+        if (self.__width is 0) or (self.__height is 0):
+            return 0
+        perimeter = 2 * (self.__width + self.__height)
+        return perimeter
+
+    @property
+    def width(self):
+        """A method which is used for getting a value
+         is decorated with @property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """A method which is used for getting a value
+         is decorated with @width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be integer")
+        if value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
+        return value
+
+    @property
+    def height(self):
+        """A method which is used for getting a value
+         is decorated with @property"""
+
+    @height.setter
+    def height(self, value):
+        """A method which is used for getting a value
+         is decorated with @height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be integer")
+        if value < 0:
+            raise ValueError("height must be >=0")
+        self.__height = value
+        return value
+
+    def __str__(self):
+        """Public instance method str"""
+        x = 0
+        y = 0
+        rec = ""
+        for x in range(self.__height):
+            for y in range(self.__width):
+                rec += "#"
+            if x != self.__height - 1:
+                rec += "\n"
+            else:
+                rec += ""
+        return rec
+
+    def print(self):
+        """Public instance method print"""
+        return(str(self))
