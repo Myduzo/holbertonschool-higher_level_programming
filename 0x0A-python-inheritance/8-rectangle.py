c91d863ba1f62f0b056b9523983d474518b2013e
Myduzo <sahli.youssef@outlook.com>
2020-06-02 19:25:22 +0100

    width and height

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """BaseGeometry class"""
+    pass
+
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
