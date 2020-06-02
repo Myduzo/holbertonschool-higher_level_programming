e88ea206393ab30854f393149ba941333a84277a
Myduzo <sahli.youssef@outlook.com>
2020-06-02 20:17:47 +0100

    fixed pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,5 +3,6 @@
 
 
 def inherits_from(obj, a_class):
-    """checks if the object is an instance of a class that inherited dir or indir"""
+    """checks if the object is an instance of\
+         a class that inherited dir or indir"""
     return isinstance(obj, a_class) and type(obj) is not a_class
