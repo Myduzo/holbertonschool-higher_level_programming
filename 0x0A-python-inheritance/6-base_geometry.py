1241ee2fe7dced1c185ca5fa950c8ca8e3a4a37d
Myduzo <sahli.youssef@outlook.com>
2020-06-02 15:58:05 +0100

    area(self)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    pass
+
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
