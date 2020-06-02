6c6216763a22dbefa0882d856c79e4da8f053302
Myduzo <sahli.youssef@outlook.com>
2020-06-02 15:21:23 +0100

    inherits

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""inherits from"""
+
+
+def inherits_from(obj, a_class):
+    """checks if the object is an instance of a class that inherited dir or indir"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
