0b1cc4f2dc696f8836b9994f2e627714630e591a
Myduzo <sahli.youssef@outlook.com>
2020-06-03 18:48:57 +0100

    pep8 fixed

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """write a string to a text file"""
     with open(filename, mode="w", encoding="utf-8") as f:
-        return f.write(text) 
+        return f.write(text)
