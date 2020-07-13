cbff118796b95362fbbc2828d7c28377f4e2995e
Myduzo <sahli.youssef@outlook.com>
2020-07-13 20:11:10 +0000

    Databases

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- script that creates the database hbtn_0c_0 in your MySQL server.
+-- create database.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- script that deletes the database hbtn_0c_0 in your MySQL server.
+-- DROP database.
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
