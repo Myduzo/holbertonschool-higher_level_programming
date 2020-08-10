201c2a5f9e025ee1624f9d2fa2babaa91ecd4e43
Myduzo <sahli.youssef@outlook.com>
2020-08-10 15:50:25 +0100

    ORM

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Model state fetch first"""
+
+
+from sys import argv
+from sqlalchemy import (create_engine, Table, Integer, String, Column)
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name.like('%a%')).all()
+    for my_state in state:
+        print("{}: {}".format(my_state.id, my_state.name))
+
+    session.close()
