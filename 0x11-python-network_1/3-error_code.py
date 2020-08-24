4d636c35567658323c9b5dac007c5be5f3fb0dcb
Myduzo <sahli.youssef@outlook.com>
2020-08-24 16:19:26 +0100

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that displays the body of the response (decoded in utf-8)"""
+
+import urllib.request as request
+import urllib.error as error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            body = response.read().decode("utf-8")
+            print(body)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
