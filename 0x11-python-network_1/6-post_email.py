5b024f8f74eb4b4be236a85001af10a8de41cb74
Myduzo <sahli.youssef@outlook.com>
2020-08-24 20:57:15 +0100

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that isplays the body of the response."""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
