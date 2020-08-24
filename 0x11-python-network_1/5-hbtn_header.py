210882c2dd5d254d4a9c9094296005fdb2db6e43
Myduzo <sahli.youssef@outlook.com>
2020-08-24 17:49:47 +0100

    header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that fetches https://intranet.hbtn.io/status"""
+"""script that displays the value of the variable X-Request-Id in the response header"""
 
 import requests
 import sys
