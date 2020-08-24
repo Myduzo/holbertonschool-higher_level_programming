a065965edfc1c2e8aa5c957008f85ed7dffa2581
Myduzo <sahli.youssef@outlook.com>
2020-08-24 21:04:03 +0100

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
-    response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+    response = requests.post(url, value)
+    print(response.text)
