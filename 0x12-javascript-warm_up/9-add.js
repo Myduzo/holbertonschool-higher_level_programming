0a70d755120279c6d282f26e8d67d6aed20dfbfe
Myduzo <sahli.youssef@outlook.com>
2020-09-15 23:18:34 +0100

    Task 9 done

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const num1 = Number(process.argv[2]);
-const num2 = Number(process.argv[3]);
 
 function add(a, b) {
   return a + b;
 }
+
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
 console.log(add(num1, num2));
