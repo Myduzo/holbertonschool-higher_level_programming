b1f5da63e631efdfb8d11ab0bb1962b72d11ea46
Myduzo <sahli.youssef@outlook.com>
2020-09-15 18:03:51 +0100

    Task 2 done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).length;
 
-const process = require('process');
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+if (args === 0) {
 	console.log('No argument');
-} else if (args.length === 1) {
+} else if (args === 1) {
 	console.log('Argument found');
 } else {
 	console.log('Argument found');
