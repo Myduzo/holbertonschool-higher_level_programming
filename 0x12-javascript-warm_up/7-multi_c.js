3831b46678c87eaf03187612e7db3389e24e7dcb
Myduzo <sahli.youssef@outlook.com>
2020-09-15 22:42:57 +0100

    Task 7 done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-if (isNaN(Number(process.argv[2]))) {
+const num = Number(process.argv[2]);
+
+
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < Number(process.argv[2]); i++) {
+  for (let i = 0; i < num; i++) {
 	console.log('C is fun');
   }
 }
