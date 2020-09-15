b748893af6174b8442e68d741372c124a440ac1e
Myduzo <sahli.youssef@outlook.com>
2020-09-15 18:49:11 +0100

    Task 2 done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2).length;
 if (args === 0) {
   console.log('No argument');
 } else if (args === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Argument found');
+  console.log('Argument found');
 }
