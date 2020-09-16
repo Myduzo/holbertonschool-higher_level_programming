ebdf8495e3ba94c87bbef58e8650343bd2234d47
Myduzo <sahli.youssef@outlook.com>
2020-09-16 12:25:22 +0100

    Task 11 done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const num = Number(process.argv[2]);
+const num = process.argv.slice(2);
 
-if (isNaN(num) || num === 1) {
-  console.log(0);
+if (num <= 3) {
+  console.log('0');
 } else {
   const list = process.argv.slice(2);
   const max = list.sort((a, b) => b - a)[1];
