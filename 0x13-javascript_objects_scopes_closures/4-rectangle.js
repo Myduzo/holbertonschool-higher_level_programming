16f741b959c04928c184c37f536a829333721cde
Myduzo <sahli.youssef@outlook.com>
2020-09-21 15:48:39 +0100

    Task 4 done

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,14 +6,17 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
     [this.height, this.width] = [this.width, this.height];
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
