c2d8d61bfbe1e04643bdf9ec85c9e217611e58b6
Myduzo <sahli.youssef@outlook.com>
2020-09-22 13:59:09 +0100

    Task 4 done

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/people/18';
+request.get(url, function (err, res, body) {
+  const films = JSON.parse(body).films;
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(films.length);
+  }
+});
