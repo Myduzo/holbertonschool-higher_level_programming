47624ef7f0d6f478c79e6c96681f80f948fcf1d7
Myduzo <sahli.youssef@outlook.com>
2020-09-22 12:20:35 +0100

    Task 1 done

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(filename, data, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
