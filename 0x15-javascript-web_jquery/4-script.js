12aca6f432d15423e56cc355c71d9ed7b20c2f6c
Myduzo <sahli.youssef@outlook.com>
2020-09-24 14:33:54 +0100

    Task 2 done

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+$('DIV#toggle_header').click(function () {
+  if ($('HEADER').hasClass('green')) {
+    $('HEADER').removeClass('green').addClass('red');
+  } else {
+    $('HEADER').removeClass('red').addClass('green');
+  }
+});
